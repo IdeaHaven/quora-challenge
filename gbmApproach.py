580dcf5e53d040ba58cdc68671641fc3b0add1d2
palimpsests <zakzibrat@gmail.com>
2013-07-20 17:43:22 -0700

    Working on GBM

--- gbmApproach.py
+++ gbmApproach.py
@@ -2,20 +2,29 @@ import os
 import csv as csv
 import numpy as np
 import random
- 
+
 columnNames = ['question_text', 'followers', 'name',
                'question_key', 'promoted_to', 'num_answers', '__ans__']
 
-path = os.getcwd()
 
-csv_file_object = csv.reader(open(path + '../test_parsed_from_bill.csv', 'rb'))
+dataFile = csv.reader(open('./questionThree/questionthree.csv', 'rb'))
 
-header = csv_file_object.next()
+# csv_file_object = csv.reader(open(path + '../test_parsed_from_bill.csv', 'rb'))
 
-data = []
-for row in csv_file_object:
-    data.append(row)
+header =dataFile.next()
+
+trainData = []
+testData = []
+for row in dataFile:
+    trainData.append(row[:-1])
+    testData.append(row[-1])
+
+
+
+
 data = np.array(data)
+
+
 
 X = df[df.columns - [-1]]
 Y = df[df.columns[-1]]
@@ -31,6 +40,6 @@ clf = GradientBoostingRegressor(**params).fit(x_train, y_train)
 
 mse = mean_squared_error(y_test, clf.predict(x_test))
 r2 = r2_score(y_test, clf.predict(x_test))
- 
+
 print("MSE: %.4f" % mse)
 print("R2: %.4f" % r2)
